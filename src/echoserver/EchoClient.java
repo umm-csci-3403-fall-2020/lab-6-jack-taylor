3b785c421ad95ffe1a90c2fe01d2348bfa14ea9e
Jack Perala <peralaj94@gmail.com>
2020-10-20 20:12:28 -0500

    Remove duplicate class declaration.
    Removed a duplicate class declaration after moving changes in VSCode back to vim.

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -10,27 +10,29 @@ public class EchoClient {
 	public static final int PORT_NUMBER = 6013;
 
 	// Setting up the constructor
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		EchoClient client = new EchoClient();
 		client.start();
 	
 	}
 
-   	private void start() {
+   	private void start() throws IOException {
 		
-		Socket socket = new Socket("localhost", PORT_NUMBER);
+		Socket soc = new Socket("localhost", PORT_NUMBER);
 		
-		InputStream socketInputStream = socket.getInputStream();
-		OutputStream socketOutputStream = socket.getOutputStream();
+		InputStream socketInputStream = soc.getInputStream();
+		OutputStream socketOutputStream = soc.getOutputStream();
 		
-		Thread firstThread = new Thread(socketInputStream);
-		Thread secondThread = new Thread(socketOutputStream);
+		Reader reader = new Reader(soc, socketInputStream);
+		Writer writer =  new Writer(soc, socketOutputStream);
+
+		Thread firstThread = new Thread(reader);
+		Thread secondThread = new Thread(writer);
 		
 		firstThread.start();
 		secondThread.start();
 	}
-}
 
 public class Reader implements Runnable {
 	
@@ -38,20 +40,24 @@ public class Reader implements Runnable {
 	Socket soc;
 	
 	public Reader(Socket soc, InputStream input) {
-		
 		this.soc = soc;
 		this.input = input;
 	}
 
 	public void run() {
 		
+		try{
+			int dataFeed;
 		while ((dataFeed = System.in.read()) != -1) {
 			System.out.write(dataFeed);
 			System.out.flush();
 		}
 		
 		soc.shutdownOutput();
+	} catch (IOException ioe){
+		System.out.println("We caught an unexpected exception");
 	}
+}
 }
 
 public class Writer implements Runnable {
@@ -64,11 +70,16 @@ public class Writer implements Runnable {
 		this.output = output;
 	}
 	
-	while ((dataFeed = System.in.read()) != -1) {
-		output.write(dataFeed);
-		output.flush();
+	public void run() {
+	try {
+		int dataFeed;
+		while ((dataFeed = System.in.read()) != -1) {
+			output.write(dataFeed);
+			output.flush();
+	} }catch (IOException ioe) {
+		System.out.println("We caught an unexpected exception");
 	}
-	
-	soc.shutdownOutput();
-    }
+	}
 }
+}
+
